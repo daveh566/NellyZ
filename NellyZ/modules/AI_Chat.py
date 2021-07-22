7d41c0e3bca91cc08d43812848523a4c9cf10e79
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-07-22 19:39:39 +0300

    Update AI_Chat.py

--- NellyZ/modules/AI_Chat.py
+++ NellyZ/modules/AI_Chat.py
@@ -287,7 +287,7 @@ async def inuka(client, message):
         return
 
 
-@daisyx.on_message(
+@nellyz.on_message(
     filters.regex("Nelly|nelly|Nellyz|nellyz|NellyZ")
     & ~filters.bot
     & ~filters.via_bot
