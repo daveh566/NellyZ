7e6b4f1a3a87e231585301e08efc1dfc991c1e8c
daveh566 <69384676+daveh566@users.noreply.github.com>
2021-07-30 14:32:52 +0300

    Update notes.py

--- NellyZ/modules/utils/notes.py
+++ NellyZ/modules/utils/notes.py
@@ -35,9 +35,9 @@ from telethon.errors import (
 from telethon.errors.rpcerrorlist import ChatWriteForbiddenError
 from telethon.tl.custom import Button
 
-import DaisyX.modules.utils.tmarkdown as tmarkdown
-from DaisyX import BOT_USERNAME
-from DaisyX.services.telethon import tbot
+import NellyZ.modules.utils.tmarkdown as tmarkdown
+from NellyZ import BOT_USERNAME
+from NellyZ.services.telethon import tbot
 
 from ...utils.logger import log
 from .language import get_chat_lang
